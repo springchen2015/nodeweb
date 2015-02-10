6166303f72da08c93c04295ec5f7a26491dd8109
springchen2015 <aaa10080@126.com>
2015-02-10 17:48:14 +0800

    modify server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require("http");
 var url = require("url");
+var fs = require('fs');
 
 function start(){
 	function onRequest(request,response){
 		var pathname = url.parse(request.url).pathname;
+		
 		response.writeHead(200,{"Content-type":"text/plain"});
 		response.write("hello World");
 		response.end();

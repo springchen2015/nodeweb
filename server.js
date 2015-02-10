7b947111fb6d131ea6605b4ba0eb3716a41ed103
springchen2015 <aaa10080@126.com>
2015-02-10 15:37:32 +0800

    add index.js and server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require("http");
+var url = require("url");
+
+function start(){
+	function onRequest(request,response){
+		var pathname = url.parse(request.url).pathname;
+		response.writeHead(200,{"Content-type":"text/plain"});
+		response.write("hello World");
+		response.end();
+		console.log("request path " + pathname);
+	};
+	http.createServer(onRequest).listen(8888);
+};
+exports.start = start;
